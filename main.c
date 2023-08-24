6c86b1e896c557636b366ba296029301d85f307f
gargmanik6080 <gargmanik6080@gmail.com>
2023-08-24 14:14:00 +0530

    Element Structure Added

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct element{
+    int at_num;
+    char symbol[2];
+    char name[14];
+    double at_mass;
+    char type[21];
+    double at_radius;
+    char discoverer[35];
+    int num_shell;
+};
+
+struct element getElement(int at_num){
+    
+}
